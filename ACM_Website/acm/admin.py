2734d1feaedec8cee6f78a0f17988480c7613559
Narayan <narayanspai2000@gmail.com>
2019-12-08 10:40:53 +0530

    Added dummy data, registered them in admin

--- ACM_Website/acm/admin.py
+++ ACM_Website/acm/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+# register all the models to view and edit it through admin-page
+admin.site.register(SIG)
+admin.site.register(Events)
+admin.site.register(Projects)
+admin.site.register(Special_people)
