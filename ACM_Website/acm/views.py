1ccef7ca965b81b1ebab2ee1ed17780d5326e48f
narayan <narayanspai2000@gmail.com>
2019-12-09 18:26:18 +0530

    Added main page, json files for main page

--- ACM_Website/acm/views.py
+++ ACM_Website/acm/views.py
@@ -1,26 +1,39 @@
 from django.shortcuts import render
 from .models import *
-from django.http import HttpResponse
+import json
+
 
 def home_page(request):
-	return HttpResponse('home page')
+    events = Events.objects.all()
+    special_people = Special_people.objects.all()
+
+    # opens the json file and saves the raw contents
+    data = open('acm/static/acm/json/index.json').read()
+    descriptions = json.loads(data)
+
+    context = {'events': events, 'special_people': special_people,
+               'descriptions': descriptions}
+    return render(request, 'acm/index.html', context)
+
 
 def sig_page(request, sig_id):
-  si=SIG.objects.filter(pk=sig_id)
-  sig=si[0]
-  sigo=SIG.objects.all()
-  events=Events.objects.filter(sig_id=sig_id)
-  projects=Projects.objects.filter(sig_id=sig_id)
-  contex = { 'sig': sig,'events': events, 'projects': projects, 'sigo': sigo }
-  return render(request,'acm/yantras.html',contex)
-def contact_us(request):
-  sigo=SIG.objects.all()
-  contex = {'sigo': sigo}
-  return render(request,'acm/contact_us.html',contex)
-def esp(request):
- sigo=SIG.objects.all()
- contex = {'sigo': sigo}
- return render(request,'acm/esp.html',contex) 
+    si = SIG.objects.filter(pk=sig_id)
+    sig = si[0]
+    sigo = SIG.objects.all()
+    events = Events.objects.filter(sig_id=sig_id)
+    projects = Projects.objects.filter(sig_id=sig_id)
+    context = {'sig': sig, 'events': events,
+               'projects': projects, 'sigo': sigo}
+    return render(request, 'acm/yantras.html', context)
 
-  
-  
+
+def contact_us(request):
+    sigo = SIG.objects.all()
+    context = {'sigo': sigo}
+    return render(request, 'acm/contact_us.html', context)
+
+
+def esp(request):
+    sigo = SIG.objects.all()
+    context = {'sigo': sigo}
+    return render(request, 'acm/esp.html', context)
