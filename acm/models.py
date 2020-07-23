485cf22d379743b313bd892328f9fec4b889acd2
Narayan Pai <narayanspai2000@gmail.com>
2020-07-24 01:46:11 +0530

    Edit project templates, add link field to project

--- acm/models.py
+++ acm/models.py
@@ -19,7 +19,6 @@ class Events(models.Model):
 class Projects(models.Model):
  sig_id = models.ForeignKey(SIG, on_delete=models.CASCADE)
  name = models.CharField(max_length=200)
- description = models.CharField(max_length=5000)
  display_picture = models.ImageField(upload_to='uploads/projects', null=True)
  duration_in_months = models.IntegerField(null=True)
  mentors = models.TextField()
@@ -31,6 +30,7 @@ class Projects(models.Model):
  conclusion = models.TextField()
  future_work = models.TextField()
  references = models.TextField()
+ meet_link = models.CharField(max_length=500)
 
  def __str__(self):
         return self.name
@@ -38,6 +38,7 @@ class Projects(models.Model):
 class ProjectPictures(models.Model):
  project_id = models.ForeignKey(Projects, on_delete=models.CASCADE)
  picture = models.ImageField(upload_to = 'uploads/projects', null=True)
+ title = models.TextField()
 
 class Special_people(models.Model):
  name = models.CharField(max_length=30)  #As only first name is mentioned
