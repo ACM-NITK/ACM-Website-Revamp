4b8f1ca8da585c4ae6de5bff525e3254fa7a6589
Narayan Pai <narayanspai2000@gmail.com>
2020-07-24 23:18:31 +0530

    Remove mandatory fields

--- acm/models.py
+++ acm/models.py
@@ -5,6 +5,7 @@ from django.core.validators import FileExtensionValidator
 
 class SIG(models.Model):
  name = models.CharField(max_length=200)
+
  def __str__(self):
         return self.name
 
@@ -13,40 +14,45 @@ class Events(models.Model):
  sig_id = models.ForeignKey(SIG, on_delete=models.CASCADE)
  name = models.CharField(max_length=200)
  description = models.CharField(max_length=5000)
+
  def __str__(self):
         return self.name
+
 
 class Projects(models.Model):
  sig_id = models.ForeignKey(SIG, on_delete=models.CASCADE)
  name = models.CharField(max_length=200)
- display_picture = models.ImageField(upload_to='uploads/projects', null=True, validators=[FileExtensionValidator(['jpg', 'jpeg', 'png', ])])
- duration_in_months = models.IntegerField(null=True)
- mentors = models.TextField()
- members = models.TextField()
- introduction = models.TextField()
- method = models.TextField()
- results = models.TextField()
- obstacles = models.TextField()
- conclusion = models.TextField()
- future_work = models.TextField()
- references = models.TextField()
- meet_link = models.CharField(max_length=500)
+ display_picture = models.ImageField(upload_to='uploads/projects', blank=True, null=True, validators=[
+                                     FileExtensionValidator(['jpg', 'jpeg', 'png', ])])
+ duration_in_months = models.IntegerField(blank=True)
+ mentors = models.TextField(blank=True)
+ members = models.TextField(blank=True)
+ introduction = models.TextField(blank=True)
+ method = models.TextField(blank=True)
+ results = models.TextField(blank=True)
+ obstacles = models.TextField(blank=True)
+ conclusion = models.TextField(blank=True)
+ future_work = models.TextField(blank=True)
+ references = models.TextField(blank=True)
+ meet_link = models.CharField(max_length=500, blank=True)
 
  def __str__(self):
         return self.name
 
+
 class ProjectPictures(models.Model):
  project_id = models.ForeignKey(Projects, on_delete=models.CASCADE)
- picture = models.ImageField(upload_to='uploads/projects', null=True, validators=[FileExtensionValidator(['jpg', 'jpeg', 'png', ])])
- title = models.TextField()
+ picture = models.ImageField(upload_to='uploads/projects', blank=True,null=True,
+                             validators=[FileExtensionValidator(['jpg', 'jpeg', 'png', ])])
+ title = models.TextField(blank=True)
+
 
 class Special_people(models.Model):
- name = models.CharField(max_length=30)  #As only first name is mentioned
+ name = models.CharField(max_length=30)  # As only first name is mentioned
  post = models.CharField(max_length=25)
  fb_link = models.CharField(max_length=500)
  linkedin_link = models.CharField(max_length=500)
  image_path = models.CharField(max_length=200)
+
  def __str__(self):
         return self.name
-
-
